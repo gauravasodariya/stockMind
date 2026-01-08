a946ec2581b1ab02a3a2f40d0dd0e56a35b171f0
Gaurav Asodariya <gauravasodariya44@gmail.com>
2026-01-08 12:34:45 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
